efcbf44c1d4029a4e1dd9d53d287f81fba1cf92a
max2buzz <maxbuzzer92@gmail.com>
2015-05-09 17:29:10 -0500

    First Commit

--- routes/index.js
+++ routes/index.js
@@ -9,12 +9,13 @@ function cypher(query,params,cb) {
          function(err,res) { cb(err,res.body)});
 };
 
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+//Sample Query
 router.get('/query/sample', function(req, res){
   var query="MATCH (n:Person) RETURN n, labels(n) as l LIMIT {limit}";
   var params={limit: 10};
@@ -23,15 +24,22 @@ router.get('/query/sample', function(req, res){
   cypher(query,params,cb);
 });
 
+
+//Actor Pair
 router.get('/query/actorPair', function(req, res){
-  var query="";
-  var params={limit: 10};
+
+  var query="MATCH (a:Actor)-[:ACTS_IN]->(m:Movie)<-[:ACTS_IN]-(s:Actor) where a.name={name} return a.name,s.name, count(m.title) as NoMovies ORDER BY NoMovies DESC LIMIT {limit}";
+  var params={
+    name: "Hugo Weaving",
+    limit: 10
+  };
   var cb=function(err,data) {
     res.json(data);
-  }
+  };
   cypher(query,params,cb);
 });
 
+//Actor Genre Success
 router.get('/query/actorGenre', function(req, res){
   var query="";
   var params={limit: 10};
@@ -40,7 +48,5 @@ router.get('/query/actorGenre', function(req, res){
   }
   cypher(query,params,cb);
 });
-
-
 
 module.exports = router;
